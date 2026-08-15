b854848a9002bd08ba2428b0d8fbc2f20f0d4842
company-ram <ramengineeringsteel@gmail.com>
2026-08-16 00:34:07 +0300

    Update log_in.controller.js

--- backend/controllers/log_in.controller.js
+++ backend/controllers/log_in.controller.js
@@ -50,8 +50,8 @@ const log_in = async(req,res)=>{
                 
                 res.cookie("token", token, {
                     httpOnly: true,
-                    secure: false,
-                    sameSite: "strict",
+                    secure: true,
+                    sameSite: "none",
                     maxAge: 30 * 24 * 60 * 60 * 1000
                 });
 
