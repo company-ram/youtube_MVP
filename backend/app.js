9599670629b8cbea8d1f395f6eb489e85a59bd95
company-ram <ramengineeringsteel@gmail.com>
2026-08-16 00:44:46 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,8 +8,6 @@ const cookie = require("cookie-parser")
 
 const cors = require("cors");
 
-const importYouTubeVideos = require("./services/youtubeImporter");
-
 const users = require("./models/users")
 
 const categories = require("./models/categories")
@@ -48,11 +46,6 @@ mongoose.connect(url)
     .then(async () => {
 
         console.log("MongoDB connected");
-
-        // تشغيل استيراد YouTube تلقائيًا
-        await importYouTubeVideos();
-
-        console.log("YouTube import finished");
 
     })
     .catch((e) => {
