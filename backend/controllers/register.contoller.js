b58d0ea7c654b862dc1d6fb94f3a13e92576707a
company-ram <ramengineeringsteel@gmail.com>
2026-08-16 00:33:25 +0300

    Update register.contoller.js

--- backend/controllers/register.contoller.js
+++ backend/controllers/register.contoller.js
@@ -51,8 +51,8 @@ const register = async(req,res)=>{
 
             res.cookie("token", token, {
                 httpOnly: true,
-                secure: false,
-                sameSite: "strict",
+                secure: true,
+                sameSite: "none",
                 maxAge: 30 * 24 * 60 * 60 * 1000
             });
 
