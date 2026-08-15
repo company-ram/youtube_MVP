9f326b7bddf677502d040bbd4a1b08b314cae57b
company-ram <ramengineeringsteel@gmail.com>
2026-08-15 22:01:18 +0300

    Update channels.controller.js

--- backend/controllers/channels.controller.js
+++ backend/controllers/channels.controller.js
@@ -43,11 +43,11 @@ const create_channel = async(req,res)=>{
                 userId:new_channel.userId
             },process.env.JWT_PASSWORD
         )
-        res.cookie("channel_token", token, {
+        res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
-            maxAge: 30 * 24 * 60 * 60 * 1000
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         return res.status(201).json({
