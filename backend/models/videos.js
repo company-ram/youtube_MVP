d2909e264f4e0ea864ccb309b5bba97e7a28d700
company-ram <ramengineeringsteel@gmail.com>
2026-08-15 23:12:05 +0300

    Update videos.js

--- backend/models/videos.js
+++ backend/models/videos.js
@@ -1,17 +1,41 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
-    title:String,
-    channelId:String,
-    description:String,
-    category:String,
-    videoUrl:String,
-    thumbnailUrl:String,
-    views:Number,
-    likes:Number
+const schema = mongoose.Schema(
+  {
+    videoId: {
+      type: String,
+      unique: true,
+      index: true
+    },
 
-}, {
+    title: {
+      type: String,
+      required: true
+    },
+
+    channelId: String,
+
+    description: String,
+
+    category: String,
+
+    videoUrl: String,
+
+    thumbnailUrl: String,
+
+    views: {
+      type: Number,
+      default: 0
+    },
+
+    likes: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
     timestamps: true
-})
+  }
+);
 
-module.exports = mongoose.model("video",schema)
+module.exports = mongoose.model("video", schema);
