87ad4e5c14fe318bdae988a2363b6f05ea73f4bd
company-ram <ramengineeringsteel@gmail.com>
2026-08-15 23:51:49 +0300

    Delete backend directory

--- backend/models/categories.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = mongoose.Schema({
-    name:String,
-}, {
-    timestamps: true
-})
-
-module.exports = mongoose.model("categorie",schema)
--- backend/routes/log_in.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const log_in_controller = require("../controllers/log_in.controller");
-
-router.post("/api/auth/log_in",log_in_controller)
-
-module.exports = router
