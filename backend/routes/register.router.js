51de2f0e687c662f0a0e8a7d70c7f4a5815e05b0
company-ram <ramengineeringsteel@gmail.com>
2026-08-17 10:26:07 +0300

    Delete backend directory

--- backend/routes/register.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const register_controller = require("../controllers/register.contoller");
-
-router.post("/api/auth/register",register_controller)
-
-module.exports = router
