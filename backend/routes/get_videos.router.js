7bc15179cba3257f973fe2de30e4111947888e7f
company-ram <ramengineeringsteel@gmail.com>
2026-08-17 11:03:37 +0300

    Update get_videos.router.js

--- backend/routes/get_videos.router.js
+++ backend/routes/get_videos.router.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const get_videos = require("../controllers/get_videos.controller");
+const { get_all_videos, get_video_by_id } = require("../controllers/get_videos.controller");
 
 const auth = require("../middleware/auth")
 
-router.get("/api/auth/get_videos",auth,get_videos)
+router.get("/api/auth/get_videos",auth,get_all_videos)
+router.get("/api/auth/get_video/:id",auth,get_video_by_id)
 
 module.exports = router
